f0299dc64223c7448f45ff749e5dd8657f23c3b4
Kingsguard <419512425@qq.com>
2019-03-14 10:39:43 +0800

    Update check_list.py

--- bandit/core/check_list.py
+++ bandit/core/check_list.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
 # Author:Ging'sGuard
-
+'''
+Add the name to be filtered to the corresponding type list
+'''
 checkList = {
-    'Call':['os.popen','eval'],
+    'Call':['os.popen','eval','assert','call_user_func','create_function','file_put_contents','fputs','file_put_contents','unserialize'],
     'FunctionDef':[],
 }
